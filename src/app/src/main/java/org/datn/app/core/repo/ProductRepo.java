018f7be0d1f7eb96cb07d59215dd9a736678d9bf
La Van Tho <tho.la@hebela.vn>
2023-05-02 11:26:28 +0700

    fix bug search

--- src/app/src/main/java/org/datn/app/core/repo/ProductRepo.java
+++ src/app/src/main/java/org/datn/app/core/repo/ProductRepo.java
@@ -28,12 +28,12 @@ public interface ProductRepo extends JpaRepository<Product, Long> {
             "INNER JOIN colors co ON co.color_id = pd.color_id " +
             "WHERE p.status = 1  " +
             "AND (:keyword IS NULL OR MATCH(p.product_name) AGAINST (:keyword  IN BOOLEAN MODE)) " +
-            "AND (p.brand_id IN (COALESCE(:brandIdList,p.brand_id))) " +
-            "AND (p.category_id IN (COALESCE(:categoryIdList,p.category_id))) " +
-            "AND (pd.color_id IN (COALESCE(:colorIdList,pd.color_id)))" +
-            "AND (p.material IN (COALESCE(:materialList,p.material))) " +
-            "AND (p.model IN (COALESCE(:modelList,p.model))) " +
-            "AND (pd.size_id IN (COALESCE(:sizeIdList,pd.size_id))) " +
+            "AND (COALESCE(:brandIdList) IS NULL OR p.brand_id IN (:brandIdList)) " +
+            "AND (COALESCE(:categoryIdList) IS NULL OR p.category_id IN (:categoryIdList)) " +
+            "AND (COALESCE(:colorIdList) IS NULL OR pd.color_id IN(:colorIdList))" +
+            "AND (COALESCE(:materialList) IS NULL OR p.material IN (:materialList)) " +
+            "AND (COALESCE(:materialList) IS NULL OR p.model IN (:modelList)) " +
+            "AND (COALESCE(:sizeIdList) IS NULL OR pd.size_id IN (:sizeIdList)) " +
             "AND  c.is_trash = FALSE", nativeQuery = true)
     @QueryHints(value = {@QueryHint(name = "org.hibernate.cacheable", value = "true")})
     Page<Product> findByFilter(String keyword, List<Long> brandIdList, List<Long> categoryIdList, List<Long> colorIdList, List<String> materialList, List<String> modelList, List<Long> sizeIdList, Pageable pageable);
